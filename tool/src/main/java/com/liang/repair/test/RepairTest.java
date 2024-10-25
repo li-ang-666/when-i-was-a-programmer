ed855913d53b242dbe8fc812c76ecf51f5d0641e
liang <li_ang_666@163.com>
2024-10-25 16:02:06 +0800

    1

--- tool/src/main/java/com/liang/repair/test/RepairTest.java
+++ tool/src/main/java/com/liang/repair/test/RepairTest.java
@@ -1,21 +1,18 @@
 package com.liang.repair.test;
 
-import com.liang.common.service.database.template.JdbcTemplate;
 import com.liang.repair.service.ConfigHolder;
-import com.vladsch.flexmark.html2md.converter.FlexmarkHtmlConverter;
-import com.vladsch.flexmark.util.data.MutableDataSet;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class RepairTest extends ConfigHolder {
-    public static void main(String[] args) throws Exception {
+    /*public static void main(String[] args) throws Exception {
         String content = new JdbcTemplate("104.data_bid").queryForObject("select content from company_bid where id = 57197", rs -> rs.getString(1));
         System.out.println(htmlToMd(content));
     }
-
+ø
     private static String htmlToMd(String html) {
         MutableDataSet options = new MutableDataSet();
         FlexmarkHtmlConverter converter = FlexmarkHtmlConverter.builder(options).build();
         return converter.convert(html);
-    }
+    }*/
 }

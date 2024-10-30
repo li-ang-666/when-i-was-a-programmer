59ba07603339a6fda614888c5d699c376cc5377b
liang <li_ang_666@163.com>
2024-10-30 14:03:28 +0800

    1

--- flink/src/main/java/com/liang/flink/project/group/bfs/GroupBfsService.java
+++ flink/src/main/java/com/liang/flink/project/group/bfs/GroupBfsService.java
@@ -4,7 +4,6 @@ import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.util.ObjUtil;
 import com.liang.common.dto.Config;
 import com.liang.common.util.ConfigUtils;
-import com.liang.common.util.JsonUtils;
 import com.liang.common.util.TycUtils;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
@@ -78,13 +77,6 @@ public class GroupBfsService {
                 });
             }
         }
-        result.forEach((k, v) -> {
-            log.info("{}", k);
-            v.forEach(path -> {
-                List<pathElement> pathElements = path.getPathElements();
-                log.info("\t{}", JsonUtils.toString(pathElements));
-            });
-        });
     }
 
     public interface pathElement extends Serializable {

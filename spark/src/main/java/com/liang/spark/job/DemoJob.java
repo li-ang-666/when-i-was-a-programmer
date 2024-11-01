730a3a191a2eaa2ee9dab7d49f987a0c87e7ce90
liang <li_ang_666@163.com>
2024-11-01 12:49:45 +0800

    1

---
+++ spark/src/main/java/com/liang/spark/job/DemoJob.java
@@ -0,0 +1,41 @@
+package com.liang.spark.job;
+
+import com.liang.common.dto.Config;
+import com.liang.common.service.connector.ConnectorTester;
+import com.liang.common.util.ConfigUtils;
+import com.liang.common.util.JsonUtils;
+import com.liang.spark.basic.SparkSessionFactory;
+import lombok.RequiredArgsConstructor;
+import org.apache.spark.api.java.function.ForeachPartitionFunction;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
+import java.util.Iterator;
+import java.util.Map;
+
+public class DemoJob {
+    public static void main(String[] args) {
+        SparkSession spark = SparkSessionFactory.createSpark(args);
+        Config config = ConfigUtils.getConfig();
+        spark.table("flink.product_info")
+                .repartition()
+                .foreachPartition(new DemoSink(config));
+        spark.close();
+    }
+
+    @RequiredArgsConstructor
+    private static final class DemoSink implements ForeachPartitionFunction<Row> {
+        private final Config config;
+
+        @Override
+        public void call(Iterator<Row> iterator) {
+            ConfigUtils.setConfig(config);
+            ConnectorTester connectorTester = new ConnectorTester();
+            while (iterator.hasNext()) {
+                Map<String, Object> columnMaps = JsonUtils.parseJsonObj(iterator.next().json());
+                connectorTester.invoke();
+            }
+            connectorTester.flush();
+        }
+    }
+}
